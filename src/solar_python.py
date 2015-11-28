e1ae84dc1a7d6ebe9f95d1bb364185ee5ecf6008
Mattias Andrée <maandree@operamail.com>
2015-11-28 02:00:10 +0100

    typo
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/solar_python.py
+++ src/solar_python.py
@@ -202,8 +202,8 @@ def sun_geometric_mean_longitude(t):
     #     only happen if `t` is negative, which can only happen for date
     #     times before 2000-(01)Jan-01 12:00:00 UTC par division modulo
     #     implementations with the signess of atleast the left operand.
-    #     More precively, it happens between cirka 1970-(01)Jan-11
-    #     16:09:02 UTC and cirka -374702470660351740 seconds before
+    #     More precively, it happens between circa 1970-(01)Jan-11
+    #     16:09:02 UTC and circa -374702470660351740 seconds before
     #     January 1, 1970 00:00 UTC, which is so far back in time
     #     it cannot be reliable pinned down to the right year, but it
     #     is without a shadow of a doubt looooong before the Earth
